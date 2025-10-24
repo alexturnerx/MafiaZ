a67e313de5684a1dfa98dfc620863fd8c53815c8
alexturnerx <HighamKanady59@hotmail.com>
2025-10-24 14:03:18 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMafiaGame = await deploy("MafiaGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`MafiaGame contract: `, deployedMafiaGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_mafia_game"; // id required to prevent reexecution
+func.tags = ["MafiaGame"];
